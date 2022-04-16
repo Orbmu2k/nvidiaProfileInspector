dbfcd33d654ae4d99f2ea90908e4ec2add1f5db3
FoulPlay <85binksy@gmail.com>
2022-04-16 13:09:08 +0100

    Updated assembly information.

--- nspector/Properties/AssemblyInfo.cs
+++ nspector/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NVIDIA Profile Inspector")]
-[assembly: AssemblyCopyright("©2020 by Orbmu2k")]
+[assembly: AssemblyCopyright("©2022 by Orbmu2k and Updated by Nathan Binks")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.4.1.1")]
+[assembly: AssemblyFileVersion("2.4.1.1")]
