c113b34c9d49cbeea013c5f134c5b59af44ee20f
Orbmu2k <github@orbmu2k.de>
2026-03-28 23:20:39 +0100

    about dialog additions

--- nvidiaProfileInspector/UI/ViewModels/AboutViewModel.cs
+++ nvidiaProfileInspector/UI/ViewModels/AboutViewModel.cs
@@ -1,5 +1,6 @@
 namespace nvidiaProfileInspector.UI.ViewModels
 {
+    using nvidiaProfileInspector.Common.Helper;
     using System.Collections.ObjectModel;
     using System.Diagnostics;
     using System.Reflection;
@@ -15,11 +16,29 @@ namespace nvidiaProfileInspector.UI.ViewModels
 
     public class AboutViewModel : ViewModelBase
     {
+        private bool _isAutomaticUpdateCheckEnabled;
+
         public string Version { get; }
         public ObservableCollection<Contributor> Contributors { get; } = new ObservableCollection<Contributor>();
         public string GitHubUrl => "https://github.com/Orbmu2k/nvidiaProfileInspector/releases";
+        public string GitHubRepositoryUrl => "https://github.com/Orbmu2k/nvidiaProfileInspector";
+
+        public bool IsAutomaticUpdateCheckEnabled
+        {
+            get => _isAutomaticUpdateCheckEnabled;
+            set
+            {
+                if (SetProperty(ref _isAutomaticUpdateCheckEnabled, value, nameof(IsAutomaticUpdateCheckEnabled)))
+                {
+                    var settings = UserSettings.LoadSettings();
+                    settings.DisableUpdateCheck = !value;
+                    settings.SaveSettings();
+                }
+            }
+        }
 
         public ICommand OpenGitHubCommand { get; }
+        public ICommand OpenGitHubRepositoryCommand { get; }
         public ICommand OpenContributorProfileCommand { get; }
         public bool IsUpdateAvailable { get; set; }
 
@@ -29,8 +48,10 @@ namespace nvidiaProfileInspector.UI.ViewModels
             var assembly = Assembly.GetExecutingAssembly();
             var version = assembly.GetName().Version;
             Version = $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+            IsAutomaticUpdateCheckEnabled = !UserSettings.LoadSettings().DisableUpdateCheck;
 
             OpenGitHubCommand = new RelayCommand(_ => OpenUrl(GitHubUrl));
+            OpenGitHubRepositoryCommand = new RelayCommand(_ => OpenUrl(GitHubRepositoryUrl));
             OpenContributorProfileCommand = new RelayCommand(param => OpenUrl(param as string));
 
             LoadContributors();
